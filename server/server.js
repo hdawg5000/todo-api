5bdaa0d2d91a02e40a6d0bc09f76abd2f6042b83
Hammad Naseem <hammadnsm@gmail.com>
2018-05-28 10:01:49 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,16 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.status(200).send({ todos })
+    }, (err) => { res.status(400).send(err) })
+})
+
 app.listen(3000, () => {
     console.log('Started server on port 3000')
 })
+
+module.exports = {
+    app
+}

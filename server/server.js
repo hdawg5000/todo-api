4a3b82097596d02f91c74d9ec7a579d4ace270bc
Hammad Naseem <hammadnsm@gmail.com>
2018-05-30 19:51:42 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -46,6 +46,20 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send({ error: 'Invalid ID' })
+    }
+    Todo.findByIdAndRemove(req.params.id).then((todo) => {
+        if (!todo) {
+            res.status(404).send({ error: 'Todo not found and was not deleted' })
+        } else {
+            res.status(200).send({ todo: todo })
+        }
+    }).catch((e) => {
+        res.status(400).send()
+    })
+})
 
 app.listen(port, () => {
     console.log(`Started server on port ${port}`)

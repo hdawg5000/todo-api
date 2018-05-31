1d2eda42e42a0d273d862abaa4f0fe53186dbe28
Hammad Naseem <hammadnsm@gmail.com>
2018-05-30 20:15:07 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var { ObjectID } = require('mongodb')
+const _ = require('lodash')
+const express = require('express')
+const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
 var { mongoose } = require('./db/mongoose')
 var { Todo } = require('./models/todo')
@@ -54,8 +55,36 @@ app.delete('/todos/:id', (req, res) => {
         if (!todo) {
             res.status(404).send({ error: 'Todo not found and was not deleted' })
         } else {
-            res.status(200).send({ todo: todo })
+            res.status(200).send({ todo })
         }
+    }).catch((e) => {
+        res.status(400).send()
+    })
+})
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id
+
+    // Pull certain items from the req body and ignore others
+    var body = _.pick(req.body, ['text', 'completed'])
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({ error: 'Invalid ID' })
+    }
+
+    // Update completedAt property
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime()
+    } else {
+        body.completed = false
+        body.completedAt = null
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('Todo not found and was not updated')
+        }
+        res.status(200).send({ todo })
     }).catch((e) => {
         res.status(400).send()
     })
